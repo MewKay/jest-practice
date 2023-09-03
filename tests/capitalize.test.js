f7b9a3588f0679e599045211658a5ea90250e028
MewKay <125735249+MewKay@users.noreply.github.com>
2023-09-03 21:09:39 +0300

    Create capitalize test

---
+++ tests/capitalize.test.js
@@ -0,0 +1,13 @@
+import capitalize from "../code/capitalize";
+
+test("Upper case first letter", () => {
+  expect(capitalize("capitalize")).toBe("Capitalize");
+});
+
+test("No upper case with number", () => {
+  expect(capitalize("21jumpstreet")).toBe("21jjumpstreet");
+});
+
+test("Yet another string to upper case first letter", () => {
+  expect(capitalize("retry")).toBe("retry");
+});
